4a90e2e20e36433300b8f149cd8e9fd799b099a8
akshayan <thiruakshayan98@gmail.com>
2022-10-20 16:40:15 +0530

    First commit

---
+++ src/main/java/com/ead/busbooking/entity/Bus.java
@@ -0,0 +1,12 @@
+package com.ead.busbooking.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bus {
+    private String busId;
+    private String busName;
+
+}
